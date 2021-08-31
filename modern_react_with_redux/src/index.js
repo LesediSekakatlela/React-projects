e606c346f6166354ad0a092a7c57b97a55035471
Lesedi <leseditumelo32@gmail.com>
2021-08-31 20:44:07 +0200

    jsx interpolation

--- modern_react_with_redux/src/index.js
+++ modern_react_with_redux/src/index.js
@@ -1,15 +1,23 @@
 // Import the React and ReactDom libraries
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 // Create a react component
+// {{}} - the inner curly brackets represent a java object.
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = { text: 'Click me' };
+  const labelText = 'Enter name:';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name"> {labelText} </label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor: 'blue', color: 'white'}}> {buttonText.text} </button>
+    </div>
+  );
 };
 
-// Take the react componenr and show it on the screen
+// Take the react component and show it on the screen
 ReactDOM.render(
   <App />,
   document.querySelector('#root')
