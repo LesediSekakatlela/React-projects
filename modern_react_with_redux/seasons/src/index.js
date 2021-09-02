0dac46c680ccd522b1f0f6f9477aa842f33c7d2f
Lesedi <leseditumelo32@gmail.com>
2021-09-02 17:04:59 +0200

    alternate state

--- modern_react_with_redux/seasons/src/index.js
+++ modern_react_with_redux/seasons/src/index.js
@@ -2,21 +2,13 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        // THIS IS THE ONLT TIME WE DO DIRECT ASSIGNMENT
-        // to this.state*
-        this.state = { lat: null, errorMessage: '' };
-
+        state = { lat: null, errorMessage: '' };
+    
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                // we called setstate!!!
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
+            // we called setstate!!!
+            position => this.setState({ lat: position.coords.latitude }),
+            err =>  this.setState({ errorMessage: err.message }) 
         );
     }
 
