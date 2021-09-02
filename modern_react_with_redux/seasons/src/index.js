b1ec48a727ec8ce55bc6f67144c84e2b84b59c1d
Lesedi <leseditumelo32@gmail.com>
2021-09-02 19:13:27 +0200

    breather

--- modern_react_with_redux/seasons/src/index.js
+++ modern_react_with_redux/seasons/src/index.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-        state = { lat: null, errorMessage: '' };
-    
+    state = { lat: null, errorMessage: '' };
+
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             // we called setstate!!!
             position => this.setState({ lat: position.coords.latitude }),
-            err =>  this.setState({ errorMessage: err.message }) 
+            err => this.setState({ errorMessage: err.message })
         );
     }
 
     // React says we have to define render!!
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay  lat={this.state.lat} />
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading!</div>;
+        return <Spinner message="Please accept location request" />;
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
