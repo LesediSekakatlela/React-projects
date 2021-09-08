910199c7616e623b8295689968aa9fe8807cf295
Lesedi <leseditumelo32@gmail.com>
2021-09-08 10:07:56 +0200

    only search with a term

--- modern_react_with_redux/widgets/src/components/Search.js
+++ modern_react_with_redux/widgets/src/components/Search.js
@@ -1,13 +1,43 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Search = () => {
     const [term, setTerm] = useState('');
-
-    console.log('I RUN WITH EVERY RENDER');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
-        console.log('I ONLY RUN ONCE');
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                },
+            });
+
+            setResults(data.query.search);
+        };
+
+        if (term) {
+            search();
+        }
     }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a className="ui buton" href={`https://en.wikipedia.org?curis=${result.pageid}`}>Go</a>
+                </div>
+                <div className="content">
+                    <div className="header">{result.title}</div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                </div>
+            </div>
+        );
+    });
 
     return (
         <div>
@@ -20,6 +50,9 @@ const Search = () => {
                         className="input"
                     />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     );
