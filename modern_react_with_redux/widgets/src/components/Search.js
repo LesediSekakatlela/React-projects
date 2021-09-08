8011bc5d23ddcdc7ee8020868e2e4a5d675a256a
Lesedi <leseditumelo32@gmail.com>
2021-09-08 09:25:44 +0200

    async code

---
+++ modern_react_with_redux/widgets/src/components/Search.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+const Search = () => {
+    const [term, setTerm] = useState('');
+
+    console.log('I RUN WITH EVERY RENDER');
+
+    useEffect(() => {
+        console.log('I ONLY RUN ONCE');
+    }, [term]);
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input
+                        value={term}
+                        onChange={(e) => setTerm(e.target.value)}
+                        className="input"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Search;
