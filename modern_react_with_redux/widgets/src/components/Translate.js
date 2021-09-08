b7cd246860e8d2dd7caad78ca512da3b0c8500cd
Lesedi <leseditumelo32@gmail.com>
2021-09-08 11:57:07 +0200

    translater

---
+++ modern_react_with_redux/widgets/src/components/Translate.js
@@ -0,0 +1,12 @@
+import React, {useStae} from "react";
+import Dropdown from "./Dropdown";
+
+const Translate = () => {
+    return (
+        <div>
+            <Dropdown />
+        </div>
+    );
+};
+
+export default Translate;
