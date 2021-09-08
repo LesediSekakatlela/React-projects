a13cde8e374302a70cdb13e4d73077b09f41e429
Lesedi <leseditumelo32@gmail.com>
2021-09-08 10:50:57 +0200

    JSX

--- modern_react_with_redux/widgets/src/components/Search.js
+++ modern_react_with_redux/widgets/src/components/Search.js
@@ -2,8 +2,19 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [term]);
 
     useEffect(() => {
         const search = async () => {
@@ -13,17 +24,14 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term,
+                    srsearch: debouncedTerm,
                 },
             });
 
             setResults(data.query.search);
         };
-
-        if (term) {
-            search();
-        }
-    }, [term]);
+        search();
+    }, [debouncedTerm]);
 
     const renderedResults = results.map((result) => {
         return (
