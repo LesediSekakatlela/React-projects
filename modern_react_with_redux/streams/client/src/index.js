0538871b65ccd451442d382744654f69ede7e048
Lesedi <leseditumelo32@gmail.com>
2021-09-15 15:46:19 +0200

    customizing form fields

--- modern_react_with_redux/streams/client/src/index.js
+++ modern_react_with_redux/streams/client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 
@@ -7,14 +7,11 @@ import App from './components/App';
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-    reducers,
-    composeEnhancers(applyMiddleware())
-    );
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
-ReactDom.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.querySelector('#root')
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
 );
