f6050ac29fbe2468e7ba6416a48af70bdf90b5be
Lesedi <leseditumelo32@gmail.com>
2021-09-17 09:17:31 +0200

    17-09-2021

--- modern_react_with_redux/streams/client/src/components/GoogleAuth.js
+++ modern_react_with_redux/streams/client/src/components/GoogleAuth.js
@@ -8,7 +8,7 @@ class GoogleAuth extends React.Component {
       window.gapi.client
         .init({
           clientId:
-            '797401886567-9cumct9mrt3v2va409rasa7fa6fq02hh.apps.googleusercontent.com',
+            '872209274452-0e9bnfk2ujrthbe55g4ojqv54rcatnkd.apps.googleusercontent.com',
           scope: 'email'
         })
         .then(() => {
