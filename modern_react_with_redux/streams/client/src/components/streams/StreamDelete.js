5e56a5689f0b361519109a07520024cfa5cbe911
Lesedi <leseditumelo32@gmail.com>
2021-09-19 13:53:28 +0200

    viewing streams

--- modern_react_with_redux/streams/client/src/components/streams/StreamDelete.js
+++ modern_react_with_redux/streams/client/src/components/streams/StreamDelete.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Modal from '../Modal';
 import history from '../../history';
-import { fetchStream } from '../../actions';
+import { fetchStream, deleteStream } from '../../actions';
 
 class StreamDelete extends React.Component {
   componentDidMount() {
@@ -10,14 +11,16 @@ class StreamDelete extends React.Component {
   }
 
   renderActions() {
+    const { id } = this.props.match.params;
+
     return (
       <React.Fragment>
-        <button className="ui button negative">Delete</button>
-        <button className="ui button">Cancel</button>
+        <button onClick={() => this.props.deleteStream(id)} className="ui button negative">Delete</button>
+        <Link to="/" className="ui button">Cancel</Link>
       </React.Fragment>
     );
   }
-  
+
   renderContent() {
     if (!this.props.stream) {
       return 'Are you sure you want to delete this stream?'
@@ -28,12 +31,12 @@ class StreamDelete extends React.Component {
 
   render() {
     return (
-        < Modal
-          title="Delete Stream"
-          content={this.renderContent()}
-          actions={this.renderActions()}
-          onDismiss={() => history.push('/')}
-        />
+      < Modal
+        title="Delete Stream"
+        content={this.renderContent()}
+        actions={this.renderActions()}
+        onDismiss={() => history.push('/')}
+      />
     );
   }
 }
@@ -42,4 +45,4 @@ const mapStateProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] }
 };
 
-export default connect(mapStateProps, { fetchStream })(StreamDelete);
+export default connect(mapStateProps, { fetchStream, deleteStream })(StreamDelete);
