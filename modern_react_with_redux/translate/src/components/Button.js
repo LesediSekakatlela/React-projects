0cf0f58b211e4513c943c491c7ffb71c54fa35a1
Lesedi <leseditumelo32@gmail.com>
2021-09-21 08:52:00 +0200

    pulling from multiple context

--- modern_react_with_redux/translate/src/components/Button.js
+++ modern_react_with_redux/translate/src/components/Button.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
-    render() {
-        return <button className="ui button primary">Submit</button>;
-    }
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen';
+  }
+
+  render() {
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {value => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
 }
 
 export default Button;
